9760b47304e9bcf33565bed84416c2e7701cd146
sorc97 <nnd64809200@gmail.com>
2020-02-02 13:03:26 +0300

    Updates

--- src/components/context/TodosPageContext.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react'
-
-interface TodosPageContextData {
-  setDragFromTodo: (todoId: string) => void
-}
-
-export const TodosPageContext = createContext<TodosPageContextData | null>(null);
--- src/helpers/array-helpers.ts
+++
@@ -1,9 +0,0 @@
-export const insert = (arr: any, index: number, ...newElements: any): Array<any> => [
-  ...arr.slice(0, index),
-  ...newElements,
-  ...arr.slice(index)
-]
-
-export const findElementById = ( id: string, arr: Array<any> ) : any => {
-  return arr.filter(item => item.id === id)[0];
-}
